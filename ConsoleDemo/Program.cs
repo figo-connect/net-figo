8e0e8f3efdec97b4252677b99e2ef889d8a7aa4a
makst <mt@figo.io>
2016-07-28 12:47:46 +0200

    start using OnRequestInitialize in console demo

--- ConsoleDemo/Program.cs
+++ ConsoleDemo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using figo;
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,23 +9,65 @@ using System.Threading.Tasks;
 
 namespace figo.ConsoleDemo {
     class Program {
-        static void Main(string[] args)  {
-            FigoSession session = new FigoSession { AccessToken = "ASHWLIkouP2O6_bgA2wWReRhletgWKHYjLqDaqb0LFfamim9RjexTo22ujRIP_cjLiRiSyQXyt2kM1eXU2XLFZQ0Hro15HikJQT_eNeT_9XQ" };
+        static void WaitTask(Task task)
+        {
+            try
+            {
+                task.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var e in ae.InnerExceptions)
+                {
+                    // Handle only WebException
+                    if (e is WebException)
+                    {
+                        Console.WriteLine("Message: {0}\nSource: {1}\nStackTrace: {2}", e.Message, e.Source, e.StackTrace);
+                        Console.ReadLine();
+                        Environment.Exit(1);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
 
-		    // print out a list of accounts including its balance
+        static void Main(string[] args)  {
+            var validCertificates = new string[]
+            {
+                "38AE4A326F16EA1581338BB0D8E4A635E727F107",
+                "DBE2E9158FC9903084FE36CAA61138D85A205D93"
+            };
+            FigoSession session = new FigoSession {
+                AccessToken = "ASHWLIkouP2O6_bgA2wWReRhletgWKHYjLqDaqb0LFfamim9RjexTo22ujRIP_cjLiRiSyQXyt2kM1eXU2XLFZQ0Hro15HikJQT_eNeT_9XQ",
+                // the same as default one here, but can be modified for testing purposes
+                ApiEndpoint = "https://api.figo.me",
+                OnRequestInitialize = (request) =>
+                {
+                    // figo.net is portable, not all of the platforms have a support for ServerCertificateValidationCallback
+                    // that's why check certificate on the client side 
+                    request.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => {
+                        X509Certificate x509Certificate = cert;
+                        return validCertificates.Contains(x509Certificate.GetCertHashString());
+                    };
+                }
+            };
+            // print out a list of accounts including its balance
             var task_accounts = session.GetAccounts();
-            task_accounts.Wait();
+            WaitTask(task_accounts);
             foreach(FigoAccount account in task_accounts.Result) {
                 Console.WriteLine(account.Name);
 
                 var task_balance = session.GetAccountBalance(account);
-                task_balance.Wait();
+                WaitTask(task_balance);
                 Console.WriteLine(task_balance.Result.Balance);
 		    }
 
 		    // print out the list of all transactions on a specific account
             var task_transactions = session.GetTransactions("A1.2");
-            task_transactions.Wait();
+            WaitTask(task_transactions);
 		    foreach(FigoTransaction transaction in task_transactions.Result) {
 			    Console.WriteLine(transaction.Purpose);
 		    }
